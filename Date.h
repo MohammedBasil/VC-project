3118ff43a4e013b366ae884cf674a123dbbbbfaa
MohammedBasil <mbasil98@hotmail.com>
2018-11-03 19:11:52 +0100

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,1 +1,22 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Date
+{
+private:
+    int month, day, year;
+public:
+    Date();
+    Date(int _m, int _d, int _y);
+    ~Date();
+    void setDay(int _d);
+    void setMonth(int _m);
+    void setYear(int _y);
+    void showDate();
+
+#endif // DATE_H
 
