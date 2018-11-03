bad1fd3290b780c72b1c6825680720a3eff778c8
MohammedBasil <mbasil98@hotmail.com>
2018-11-03 14:39:11 +0100

    Update UniversityMember.h

--- UniversityMember.h
+++ UniversityMember.h
@@ -1,1 +1,28 @@
+#ifndef UNIVERSITYMEMBER_H
+#define UNIVERSITYMEMBER_H
+
+#include <iostream>
+#include<string>
+#include “virtual_campus.h”
+
+using namespace std;
+
+class UniversityMember
+{
+public:
+    UniversityMember();
+
+private:
+    string name;
+    string surname;
+    string surname2;
+    char ID[7];
+    char type;
+
+
+};
+
+
+
+#endif // UNIVERSITYMEMBER_H
 
